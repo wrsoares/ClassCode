06f40fa6703d10cab9aeb42f6719b3df32a962f1
wrsoares <william.rosendo.soares@gmail.com>
2022-10-17 20:52:52 -0300

    first commit

---
+++ src/main/java/CodeClass.java
@@ -0,0 +1,15 @@
+public class CodeClass {
+
+    String className = "First";
+    String qtdStudents;
+
+    public String setClassName(String className) {
+        this.className = className;
+        return this.className;
+    }
+
+    public void setQtdStudents(String qtdStudents) {
+        this.qtdStudents = qtdStudents;
+    }
+
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,23 @@
+public class Runner {
+    /*
+     * Author: William Soares
+     * Date: 17/10/2022
+     * Description: Executor classes instances
+     * */
+    public static void main(String[] args) {
+        //Criando instâncias (Objetos) da classe CodeClass
+        CodeClass firstClass = new CodeClass();
+        CodeClass secondClass = new CodeClass();
+        CodeClass thirdClass = new CodeClass();
+
+        //Manipulando o retorno do método setClassName
+        String value = firstClass.setClassName("First");
+        String secondValue = secondClass.setClassName("Second");
+
+        //Imprimindo os valores de retorno
+        System.out.println(value);
+        System.out.println(secondValue);
+        System.out.println(thirdClass.setClassName("Third"));
+    }
+
+}
